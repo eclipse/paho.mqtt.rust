6043a1994fa91e9448096cdab6bf5c99755e792a
Frank Pagliughi <fpagliughi@mindspring.com>
2017-12-26 14:21:10 -0500

    Added unit tests for CreateOptions

--- src/create_options.rs
+++ src/create_options.rs
@@ -20,29 +20,42 @@
  *    Frank Pagliughi - initial implementation and documentation
  *******************************************************************************/
 
+use std::fmt;
+
 use ffi;
 use client_persistence::ClientPersistence;
 
 /*
-pub const MQTTCLIENT_PERSISTENCE_DEFAULT: ::std::os::raw::c_uint = 0;
-pub const MQTTCLIENT_PERSISTENCE_NONE:    ::std::os::raw::c_uint = 1;
-pub const MQTTCLIENT_PERSISTENCE_USER:    ::std::os::raw::c_uint = 2;
+Remember the C constants (c_uint)
+  MQTTCLIENT_PERSISTENCE_DEFAULT = 0
+  MQTTCLIENT_PERSISTENCE_NONE    = 1
+  MQTTCLIENT_PERSISTENCE_USER    = 2
 */
 
-//#[derive(PartialEq)]
 pub enum PersistenceType {
 	File,
 	None,
 	User(Box<Box<ClientPersistence>>),
 }
 
+impl fmt::Debug for PersistenceType {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		match *self {
+			PersistenceType::File => write!(f, "File"),
+			PersistenceType::None => write!(f, "None"),
+			PersistenceType::User(_) => write!(f, "User"),
+		}
+	}
+}
+
 /////////////////////////////////////////////////////////////////////////////
-///
+//							Create Options
+/////////////////////////////////////////////////////////////////////////////
 
 /**
  * The options for creating an MQTT client.
  */
-//#[derive(Debug)]
+#[derive(Debug)]
 pub struct CreateOptions {
 	pub copts: ffi::MQTTAsync_createOptions,
 	pub server_uri: String,
@@ -145,4 +158,110 @@ impl CreateOptionsBuilder {
 		}
 	}
 }
+
+/////////////////////////////////////////////////////////////////////////////
+//								Unit Tests
+/////////////////////////////////////////////////////////////////////////////
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	// Rust options should be the same as the C options
+	#[test]
+	fn test_default() {
+		let opts = CreateOptions::default();
+		let copts = ffi::MQTTAsync_createOptions::default();
+
+		// First, make sure C options valid
+		assert_eq!([ 'M' as i8, 'Q' as i8, 'C' as i8, 'O' as i8], copts.struct_id);
+		assert_eq!(0, copts.struct_version);	// Currently supported version
+
+		assert_eq!(copts.struct_id, opts.copts.struct_id);
+		assert_eq!(copts.struct_version, opts.copts.struct_version);
+		assert_eq!(copts.sendWhileDisconnected, opts.copts.sendWhileDisconnected);
+		assert_eq!(copts.maxBufferedMessages, opts.copts.maxBufferedMessages);
+
+		assert_eq!("", &opts.server_uri);
+		assert_eq!("", &opts.client_id);
+		//assert_eq!(PersistenceType::File, opts.persistence);
+	}
+
+	#[test]
+	fn test_from_string() {
+		const HOST: &'static str = "localhost";
+		let opts = CreateOptions::from(HOST);
+		let copts = ffi::MQTTAsync_createOptions::default();
+
+		assert_eq!([ 'M' as i8, 'Q' as i8, 'C' as i8, 'O' as i8], opts.copts.struct_id);
+		assert_eq!(0, opts.copts.struct_version);	// Currently supported version
+		assert_eq!(copts.sendWhileDisconnected, opts.copts.sendWhileDisconnected);
+		assert_eq!(copts.maxBufferedMessages, opts.copts.maxBufferedMessages);
+
+		assert_eq!(HOST, &opts.server_uri);
+		assert_eq!("", &opts.client_id);
+		//assert_eq!(PersistenceType::File, opts.persistence);
+	}
+
+
+	#[test]
+	fn test_from_tuple() {
+		const HOST: &'static str = "localhost";
+		const ID: &'static str = "bubba";
+		let opts = CreateOptions::from((HOST,ID));
+		let copts = ffi::MQTTAsync_createOptions::default();
+
+		assert_eq!([ 'M' as i8, 'Q' as i8, 'C' as i8, 'O' as i8], opts.copts.struct_id);
+		assert_eq!(0, opts.copts.struct_version);	// Currently supported version
+		assert_eq!(copts.sendWhileDisconnected, opts.copts.sendWhileDisconnected);
+		assert_eq!(copts.maxBufferedMessages, opts.copts.maxBufferedMessages);
+
+		assert_eq!(HOST, &opts.server_uri);
+		assert_eq!(ID, &opts.client_id);
+		//assert_eq!(PersistenceType::File, opts.persistence);
+	}
+
+	#[test]
+	fn test_defaultbuilder() {
+		let opts = CreateOptionsBuilder::new().finalize();
+		let copts = ffi::MQTTAsync_createOptions::default();
+
+		// First, make sure C options valid
+		assert_eq!([ 'M' as i8, 'Q' as i8, 'C' as i8, 'O' as i8], copts.struct_id);
+		assert_eq!(0, copts.struct_version);	// Currently supported version
+
+		assert_eq!(copts.struct_id, opts.copts.struct_id);
+		assert_eq!(copts.struct_version, opts.copts.struct_version);
+		assert_eq!(copts.sendWhileDisconnected, opts.copts.sendWhileDisconnected);
+		assert_eq!(copts.maxBufferedMessages, opts.copts.maxBufferedMessages);
+
+		assert_eq!("", &opts.server_uri);
+		assert_eq!("", &opts.client_id);
+		//assert_eq!(PersistenceType::File, opts.persistence);
+	}
+
+	#[test]
+	fn test_builder() {
+		const HOST: &'static str = "localhost";
+		const ID: &'static str = "bubba";
+		const MAX_BUF_MSGS: i32 = 100;
+
+		let opts = CreateOptionsBuilder::new()
+						.server_uri(HOST)
+						.client_id(ID)
+						// TODO: Test persistence
+						.max_buffered_messages(MAX_BUF_MSGS)
+						.finalize();
+
+		assert_eq!([ 'M' as i8, 'Q' as i8, 'C' as i8, 'O' as i8], opts.copts.struct_id);
+		assert_eq!(0, opts.copts.struct_version);	// Currently supported version
+
+		assert_eq!(HOST, &opts.server_uri);
+		assert_eq!(ID, &opts.client_id);
+		//assert_eq!(PersistenceType::File, opts.persistence);
+		assert!(0 != opts.copts.sendWhileDisconnected);
+		assert_eq!(MAX_BUF_MSGS, opts.copts.maxBufferedMessages);
+	}
+}
+
 
