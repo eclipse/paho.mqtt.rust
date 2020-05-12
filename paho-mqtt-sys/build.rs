1694ff1c9c53e26835a3a3de8d33c391ff81a447
Bernd Vogt <bernd.vogt@zf.com>
2020-05-12 19:53:21 +0200

    #72: Add option 'OPENSSL_LINK_STATIC' to link SSL libs statically

--- paho-mqtt-sys/build.rs
+++ paho-mqtt-sys/build.rs
@@ -175,6 +175,7 @@ mod build {
     use std::path::Path;
     use std::process::Command;
     use std::env;
+    use std::str::FromStr;
 
     pub fn main() {
         println!("debug:Running the bundled build for Paho C");
@@ -250,9 +251,19 @@ mod build {
 
         // Link in the SSL libraries if configured for it.
         if cfg!(feature = "ssl") {
+            let ssl_link_static = match env::var("OPENSSL_LINK_STATIC") {
+                Ok(value) => bool::from_str(value.as_str()).unwrap(),
+                Err(_) => cfg!(windows),
+            };
+
+            let ssl_link_kind = match ssl_link_static {
+                true => "static",
+                false => "dylib",
+            };
+
             if cfg!(windows) {
-                println!("cargo:rustc-link-lib=libssl");
-                println!("cargo:rustc-link-lib=libcrypto");
+                println!("cargo:rustc-link-lib={}=libssl", ssl_link_kind);
+                println!("cargo:rustc-link-lib={}=libcrypto", ssl_link_kind);
                 if let Ok(ssl_sp) = env::var("OPENSSL_ROOT_DIR") {
                     println!("cargo:rustc-link-search={}\\lib", ssl_sp);
                 }
@@ -265,8 +276,8 @@ mod build {
                 };
             }
             else {
-                println!("cargo:rustc-link-lib=ssl");
-                println!("cargo:rustc-link-lib=crypto");
+                println!("cargo:rustc-link-lib={}=ssl", ssl_link_kind);
+                println!("cargo:rustc-link-lib={}=crypto", ssl_link_kind);
                 if let Ok(ssl_sp) = env::var("OPENSSL_ROOT_DIR") {
                     println!("cargo:rustc-link-search={}/lib", ssl_sp);
                 }
@@ -340,4 +351,3 @@ mod build {
         bindings::place_bindings(&Path::new(&inc_dir));
     }
 }
-
