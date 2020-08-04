7d406c1418353d8467b5e61e189feca5690f2f2e
fpagliughi <fpagliughi@mindspring.com>
2020-08-04 00:35:23 -0400

    Added alias capability to the topic struct.

--- src/topic.rs
+++ src/topic.rs
@@ -28,7 +28,8 @@ use crate::{
         DeliveryToken,
     },
     subscribe_options::SubscribeOptions,
-    message::Message,
+    message::{Message, MessageBuilder},
+    properties::PropertyCode,
 };
 
 /////////////////////////////////////////////////////////////////////////////
@@ -47,6 +48,9 @@ pub struct Topic<'a> {
     qos: i32,
     /// Whether the last message should be retained by the broker.
     retained: bool,
+    /// The topic alias.
+    /// If this is non-zero is it sent in place of the string topic name.
+    alias: u16,
 }
 
 impl<'a> Topic<'a> 
@@ -67,6 +71,7 @@ impl<'a> Topic<'a>
             topic: topic.into(),
             qos,
             retained: false,
+            alias: 0,
         }
     }
 
@@ -86,6 +91,7 @@ impl<'a> Topic<'a>
             topic: topic.into(),
             qos,
             retained: true,
+            alias: 0,
         }
     }
 
@@ -103,6 +109,10 @@ impl<'a> Topic<'a>
 
     /// Publish a message on the topic.
     ///
+    /// If a topic alias was previously sent, this will use the integer alias
+    /// property instead of sending the topic string.
+    /// Topis aliases are only applicable for MQTT v5 connections.
+    ///
     /// # Arguments
     ///
     /// `payload` The payload of the message
@@ -111,7 +121,57 @@ impl<'a> Topic<'a>
         where V: Into<Vec<u8>>
     {
         // OPTIMIZE: This could be more efficient.
-        let msg = Message::new(self.topic.clone(), payload, self.qos);
+        let msg = if self.alias == 0 {
+            Message::new(&self.topic, payload, self.qos)
+        }
+        else {
+            let props = properties!{ PropertyCode::TopicAlias => self.alias };
+            MessageBuilder::new()
+                .topic("")
+                .payload(payload)
+                .qos(self.qos)
+                .properties(props)
+                .finalize()
+        };
+        self.cli.publish(msg)
+    }
+
+    /// Publish a message with a topic alias.
+    ///
+    /// This publishes the message with a topic alias property to set the
+    /// alias at the broker. After calling this, the object keeps the
+    /// alias and uses it for subsequent publishes instead of sending the
+    /// full topic string.
+    ///
+    /// Note that using an alias is only valid with an MQTT v5 connection,
+    /// and the value must be in the range of 1 - TopicAliasMaximum as the
+    /// broker reported in the CONNACK packet. The alias is only valid
+    /// for a single connection. It should be reset on a reconnect.
+    ///
+    /// This can be called a second time to change the alias setting.
+    /// Using an alias of zero on a subsequent call instructs this object to
+    /// stop using the alias and publish with the topic name.
+    ///
+    /// # Arguments
+    ///
+    /// `alias` The integer alias to use for subsequent message publishing.
+    ///     This must be in the range 1 - `TopicAliasMaximum` as reported by
+    ///     the server in the CONNACK package. Using a value of zero
+    ///     instructs this object to stop using the alias and go back to
+    ///     publishing with the string topic name.
+    /// `payload` The payload of the message
+    ///
+    pub fn publish_with_alias<V>(&mut self, alias: u16, payload: V) -> DeliveryToken
+        where V: Into<Vec<u8>>
+    {
+        self.alias = alias;
+        let props = properties!{ PropertyCode::TopicAlias => alias };
+        let msg = MessageBuilder::new()
+            .topic(self.topic.clone())
+            .payload(payload)
+            .qos(self.qos)
+            .properties(props)
+            .finalize();
         self.cli.publish(msg)
     }
 }
